732f41ddde33956f0a815b891d843a3bf47ff86f
Luis Alberto Luft <luberluft@gmail.com>
2020-01-12 22:59:09 -0400

    PDF pages rework

--- laravel/resources/views/example.blade.php
+++ laravel/resources/views/example.blade.php
@@ -3,19 +3,7 @@
 @section('title', 'Fluência - Exemplo')
 
 @section('body')
-  <div class="container">
 
-    <object
-      data="https://drive.google.com/file/d/1hfJL6fE-bTWhxYoe8cFFe7VGtnvnajHH/preview"
-      type="application/pdf">
-      <embed
-        src="storage/exemplo.pdf"/>
-      <p>
-          Este navegador não suporta PDFs.
-          Por favor baixe o PDF pra vizualizá-lo com este link:
-        <a href="https://drive.google.com/file/d/1hfJL6fE-bTWhxYoe8cFFe7VGtnvnajHH/preview">Clique aqui para ver o PDF</a>.
-      </p>
-      </embed>
-    </object>
-  </div>
+    <embed src="https://drive.google.com/file/d/1hfJL6fE-bTWhxYoe8cFFe7VGtnvnajHH/preview">
+
 @endsection
