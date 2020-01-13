40ee34f83c610b3dcb304baaddafdf0dc6487c5f
Luis Alberto Luft <luberluft@gmail.com>
2020-01-12 22:56:30 -0400

    PDF testing embed

--- laravel/resources/views/theory.blade.php
+++ laravel/resources/views/theory.blade.php
@@ -4,17 +4,6 @@
 
 @section('body')
 
-  <object
-    data="https://drive.google.com/file/d/1H2xAInVduE92Zjk_9_Lc1zHQaGtC1fbg/preview"
-    type="application/pdf">
-    <embed
-      src="storage/teoria.pdf"/>
-    <p>
-      Este navegador não suporta PDFs.
-      Por favor baixe o PDF pra vizualizá-lo com este link:
-        <a href="https://drive.google.com/file/d/1H2xAInVduE92Zjk_9_Lc1zHQaGtC1fbg/preview">Clique aqui para ver o PDF</a>.
-      </p>
-    </embed>
-  </object>
+  <embed src="https://drive.google.com/file/d/1H2xAInVduE92Zjk_9_Lc1zHQaGtC1fbg/preview">
 
 @endsection
