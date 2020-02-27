ab7ed28b05ecdd7aa6724c20c688301449870cbd
Luis Alberto Luft <luberluft@gmail.com>
2020-02-27 14:10:32 -0400

    Change PHP timezone

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -12,12 +12,12 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    
+
     'name' => env(
         'APP_NAME',
         'Laravel'
     ),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -28,12 +28,12 @@ return [
     | services the application utilizes. Set this in your ".env" file.
     |
     */
-    
+
     'env' => env(
         'APP_ENV',
         'production'
     ),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -44,12 +44,12 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
-    
+
     'debug' => env(
         'APP_DEBUG',
         false
     ),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application URL
@@ -60,17 +60,17 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-    
+
     'url' => env(
         'APP_URL',
         'http://localhost'
     ),
-    
+
     'asset_url' => env(
         'ASSET_URL',
         null
     ),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -81,9 +81,9 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-    
-    'timezone' => 'UTC',
-    
+
+    'timezone' => 'America/Campo_Grande	',
+
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -94,9 +94,9 @@ return [
     | to any of the locales which will be supported by the application.
     |
     */
-    
+
     'locale' => 'en',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -107,9 +107,9 @@ return [
     | the language folders that are provided through your application.
     |
     */
-    
+
     'fallback_locale' => 'en',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Faker Locale
@@ -120,9 +120,9 @@ return [
     | localized telephone numbers, street address information and more.
     |
     */
-    
+
     'faker_locale' => 'en_US',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
@@ -133,11 +133,11 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-    
+
     'key' => env('APP_KEY'),
-    
+
     'cipher' => 'AES-256-CBC',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
@@ -148,9 +148,9 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
-    
+
     'providers' => [
-        
+
         /*
          * Laravel Framework Service Providers...
          */
@@ -176,11 +176,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        
+
         /*
          * Package Service Providers...
          */
-        
+
         /*
          * Application Service Providers...
          */
@@ -189,9 +189,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-    
+
     ],
-    
+
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
@@ -202,9 +202,9 @@ return [
     | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
-    
+
     'aliases' => [
-        
+
         'App'          => Illuminate\Support\Facades\App::class,
         'Arr'          => Illuminate\Support\Arr::class,
         'Artisan'      => Illuminate\Support\Facades\Artisan::class,
@@ -240,7 +240,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-    
+
     ],
 
 ];
